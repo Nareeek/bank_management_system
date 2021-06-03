6b9c18dedce265e5f5c16b1788cc6819c35cec25
Narek <narek.ikhtiaryan.ni@gmail.com>
2021-06-03 07:57:11 -0700

    turn all class member variables private

--- customer.h
+++ customer.h
@@ -2,14 +2,25 @@
 
 class Customer{
   public:
+    Customer(std::string n, std::string sn, std::string _id, std::string l, std::string p);
+
+    std::string get_name(){ return name;};
+    std::string get_surname(){ return surname;};
+    std::string get_id(){ return id;};
+    std::string get_login(){ return login;};
+    std::string get_password(){ return password;};
+    
+    long long get_bank_account(){ return bank_account;};
+    long get_balance(){ return balance;};
+    
+
+  private:
     std::string name;
     std::string surname;
     std::string id;
     std::string login;
     std::string password;
     long long bank_account;
-    long balance;
-
-    Customer(std::string n, std::string sn, std::string _id, std::string l, std::string p);
-    
+    long balance;   
 };
+
